279b3353dddae883b98957cffb5317eef7552b68
Rose Akinyi <representativeakinyi@gmail.com>
2026-04-24 18:54:10 +0300

    fix main block properly

--- app.py
+++ app.py
@@ -56,9 +56,6 @@ def predict():
         confidence=f"{confidence:.2f}",
         image=filepath
     )
-
 if __name__ == "__main__":
-    app.run(debug=True)
-    if __name__ == "__main__":
-        print("APP STARTING...")   # ← add this line
-        app.run(host="0.0.0.0", port=5000)
+    print("APP STARTING...")
+    app.run(host="0.0.0.0", port=5000)
